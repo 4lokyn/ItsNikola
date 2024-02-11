f9fd8b0de7257f041607b0bfc33f81370f391384
Nikola Pantovic <nikol4pantovic@gmail.com>
2024-02-11 12:00:03 -0600

    navigation html/scss with responsive desigh compleated

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './app-components/home/home.component';
+import { SkillsComponent } from './app-components/skills/skills.component';
+import { AboutComponent } from './app-components/about/about.component';
+import { ContactComponent } from './app-components/contact/contact.component';
+import { NotFoundComponent } from './app-components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Home',component:HomeComponent},
+  {path:'Skills',component:SkillsComponent},
+  {path:'About',component:AboutComponent},
+  {path:'Contact',component:ContactComponent},
+  {path:'',redirectTo: 'Home', pathMatch:'full'},
+  {path:'**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './app-components/about/about.component';
+import { ContactComponent } from './app-components/contact/contact.component';
+import { HomeComponent } from './app-components/home/home.component';
+import { NavigationComponent } from './app-components/navigation/navigation.component';
+import { NotFoundComponent } from './app-components/not-found/not-found.component';
+import { SkillsComponent } from './app-components/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    NavigationComponent,
+    NotFoundComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
